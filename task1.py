7df6619c9d649aab321fe783dc16750476fa18c6
rakshitha <155514363+rakshiithaaa@users.noreply.github.com>
2024-06-23 19:16:14 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,38 @@
+#creating a list
+my_list = [1,2,3,4,5]
+
+#adding an element to the list
+my_list.append(6)
+
+#removing an element from the list
+my_list.remove(3)
+
+#modifying an element from the list
+my_list[0] = 10
+
+print("updated list:" , my_list);
+
+#create dictionary
+my_dict = {'name':'john','age':20,'city':'delhi'}
+
+#adding
+my_dict['gender'] = 'male'
+
+#removing
+my_dict['age']
+
+#modifying
+my_dict['city'] = 'blr'
+print("updated dictionary:" , my_dict)
+
+#creating set
+my_set = {1,2,3,4,5}
+#adding
+my_set.add(6)
+#removing
+my_set.remove(3)
+#modifying
+my_set.discard(1)
+my_set.add(10)
+print("updated set:",my_set)
+
